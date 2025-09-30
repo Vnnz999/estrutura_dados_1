552e2f87abb768143fe8ed50a012ec15823b2e35
vinicius <viniciuscaetano200050000@gmail.com>
2025-09-30 20:02:14 -0300

    ativ dia24

---
+++ ATIV4_DIA23.java
@@ -0,0 +1,6 @@
+import java.util.Vector;
+
+public class ATIV4_DIA23 {
+    public static void main(String[] args) {
+        System.out.println("A principal diferença é:\n Vector é uma implementação de uma lista dinamica\n⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜\n enquanto stack é uma implementação de pilha"); 
+    }}
