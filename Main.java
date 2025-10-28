85262038ad658ee14e87c11bf40ecf8d7988def8
vinicius <viniciuscaetano200050000@gmail.com>
2025-10-28 20:17:41 -0300

    atividade dia 28/10

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("************RESPOSTAS**************\n1(A)\n2(B)\n3(A)\n4(A)\n5(C)\n6(B)\n7(B)\n8(A)\n9(A)\n10(B)");
+		}
+}
+
+
