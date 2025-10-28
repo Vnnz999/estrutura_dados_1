bd54c3106fed82bacc237a2e70229dde21149faf
vinicius <viniciuscaetano200050000@gmail.com>
2025-10-28 20:44:03 -0300

    Add files via upload

---
+++ ATIV_2810.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ATIV_2810
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    
+		System.out.println("************RESPOSTAS**************\n1(B)\n2(A)\n3(B)\n4(B)\n5(B)\n6(A)\n7(B)\n8(C)\n9(C)\n10(A)");
+		}
+}
+
+
